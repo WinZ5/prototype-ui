fabe6a73576ef7c426a792fc24aac334d982d8ed
SP.Win <supawit.promma6256@gmail.com>
2023-12-08 01:30:01 +0700

    change incorrect import

--- app/components/login.tsx
+++ app/components/login.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@radix-ui/react-label";
+import { Label } from "@/components/ui/label";
 
 import { useRouter } from "next/navigation"; 
 
