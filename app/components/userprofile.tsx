2e1b54b28372c849e864b3b32c7e367a9be812c0
SP.Win <supawit.promma6256@gmail.com>
2023-12-08 23:40:12 +0700

    add userprofile

---
+++ app/components/userprofile.tsx
@@ -0,0 +1,53 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Avatar,
+  AvatarFallback,
+} from "@/components/ui/avatar";
+
+export function UserProfile() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+          <Avatar className="h-9 w-9">
+            <AvatarFallback>JD</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">
+              John Jimmy Jackson Doe
+            </p>
+            <p className="text-xs leading-none text-muted-foreground">
+              test@example.com
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            Profile
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Settings
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,11 @@
+import { UserProfile } from "../components/userprofile";
+
 export default function Project() {
-    return (
-        <main>
-            Project
-        </main>
-    )
+  return (
+    <div className="h-full flex-1 flex-col space-y-8 p-8">
+      <div className="flex items-center justify-end space-x-2">
+        <UserProfile />
+      </div>
+    </div>
+  )
 }
